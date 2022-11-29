d7126c5f839fed1870b9946f4aa87b3aa7a5215b
Kritarth2002 <98588519+Kritarth2002@users.noreply.github.com>
2022-11-29 15:53:24 +0530

    First commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
-const fs = require('fs');
+const fs = require("fs");
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -15,20 +15,22 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     goerli: {
-      url: "<YOUR_ALCHEMY_URL>",
-      accounts: [ "<YOUR_PRIVATE_KEY>" ]
-    }
+      url: "https://eth-goerli.g.alchemy.com/v2/-VTrp56mvQyse2g7auFZWukuHgtTwBHE",
+      accounts: [
+        "a376fe04f4778f5c615c0b6f53176744425d8f626fc806c393a8ac3b8ac47915",
+      ],
+    },
   },
   solidity: {
     version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
+        runs: 200,
+      },
+    },
+  },
 };
